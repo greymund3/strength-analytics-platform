8a50d39c353a9f95e68537a51069c1dfc5783b38
Reymundo Guerrero <164686226+greymund3@users.noreply.github.com>
2026-05-26 16:41:10 -0500

    Initial Commit

---
+++ strength-analytics-platform/strength_analytics_platformApp.swift
@@ -0,0 +1,17 @@
+//
+//  strength_analytics_platformApp.swift
+//  strength-analytics-platform
+//
+//  Created by Reymundo Jr Guerrero on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct strength_analytics_platformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
